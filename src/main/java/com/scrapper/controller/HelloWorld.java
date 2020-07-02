17c9ec0060608c5538d695a20dacb9e2212ee9eb
Awais Kazi <awais.qazi@visual-meta.com>
2020-07-03 01:47:55 +0200

    fixed controller

--- src/main/java/com/scrapper/controller/HelloWorld.java
+++ src/main/java/com/scrapper/controller/HelloWorld.java
@@ -2,6 +2,7 @@ package com.scrapper.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
@@ -10,17 +11,17 @@ import java.lang.management.OperatingSystemMXBean;
 public class HelloWorld {
 
     @RequestMapping("/")
-    public String getHellowWorld()
+    public @ResponseBody String getHellowWorld()
     {
         OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(
                 OperatingSystemMXBean.class);
 
         StringBuilder sb = new StringBuilder();
-        sb.append("Hello World from the Free Server!\n");
-        sb.append("Currently System Load Average is " + osBean.getSystemLoadAverage() + "\n");
-        sb.append("Currently Available Processors are" + osBean.getAvailableProcessors());
+        sb.append("Hello World from the Free Server!</br>");
+        sb.append("\nCurrently System Load Average is " + osBean.getSystemLoadAverage() + "</br>");
+        sb.append("Currently Available Processors are " + osBean.getAvailableProcessors());
 
-        return "";
+        return sb.toString();
 
     }
 
